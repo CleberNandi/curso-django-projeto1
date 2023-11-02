d3da2b5814534bbb8d35810b099ea4e54687f72f
Cleber Goulart Nandi <cleber.nandi@gmail.com>
2023-11-02 15:41:24 -0400

    Alterando urls geral para incluir media_url e media_roor e static

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 app_name = "recipes"
@@ -7,3 +9,7 @@ urlpatterns = [
     path("", views.home_view, name="home"),
     path("recipes/<int:id>/", views.recipe_view, name="recipe"),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
