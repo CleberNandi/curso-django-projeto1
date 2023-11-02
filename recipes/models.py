01040e692c68f2904e95710a31acb37f65c20e1d
Cleber Goulart Nandi <cleber.nandi@gmail.com>
2023-11-02 15:08:02 -0400

    Criado Tabela de Category e as relacoes enre Recipe e category e users

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
 
 
 class Recipe(models.Model):
@@ -15,3 +20,5 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     ìs_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to="recipes/cover/%Y-%m-%d")
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
