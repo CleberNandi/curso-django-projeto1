fd3416f626fef5362d032ee91783eacad84f5ed3
Cleber Goulart Nandi <cleber.nandi@gmail.com>
2023-11-02 14:50:28 -0400

    Alterando views

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
+
 def home_view(request):
-    return render(request, "recipes/pages/home.html", status=200, context={
-        'name': 'Cleber Goulart Nandi'
-    })
+    return render(
+        request,
+        "recipes/pages/home_view.html",
+        status=200,
+        context={"recipes": [make_recipe() for _ in range(10)]},
+    )
+
+
+def recipe_view(request, id):
+    return render(
+        request,
+        "recipes/pages/recipe_view.html",
+        status=200,
+        context={
+            "recipe": make_recipe(),
+            "is_detail_page": True,
+        },
+    )
